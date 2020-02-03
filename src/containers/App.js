84abd3fb5561180441066792b30f4c078e8dba2c
Admin <you@example.com>
2020-02-03 16:04:35 +0200

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './App.css';
+import Quiz from "../components/Quiz/quiz";
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect
+} from "react-router-dom";
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            step: 1,
+            answers: {}
+        };
+    }
+    // componentDidMount() {
+    //
+    // }
+    render() {
+        return (
+            <div className="container">
+                <Router>
+                    <Route exact path="/quiz/:id" component={Quiz}/>
+                    <Redirect from="/" to="/quiz" />
+                </Router>
+            </div>
+        )
+    };
+}
+export default App;
